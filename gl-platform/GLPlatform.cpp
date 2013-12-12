75673accc4a87683935957234e9c196225c8a413
James Hughes <jhughes@sci.utah.edu>
2013-12-11 18:06:22 -0700

    Add exception if GLEW fails to initialize.

--- gl-platform/GLPlatform.cpp
+++ gl-platform/GLPlatform.cpp
@@ -1,10 +1,15 @@
 
 #include "GLPlatform.hpp"
+#include <stdexcept>
 
 void glPlatformInit()
 {
 #ifdef GL_PLATFORM_USING_WIN
-  glewInit();
+  GLenum err = glewInit();
+  if (GLEW_OK != err)
+  {
+    throw std::runtime_error("GLEW failed to initialize.");
+  }
 #endif
 }
 
